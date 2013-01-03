7d177a2e05fa733e2639b9346c2ca51d9107b0f4
root <root@d3v11.ano>
2013-01-03 09:17:19 +0000

    [keypair.c] (new)

---
+++ src/keypair.c
@@ -0,0 +1,20 @@
+#include <nacl/crypto_box.h>
+#include <time.h>
+int main(){
+unsigned char pk [32];
+unsigned char sk [32];
+          int  i     ;
+
+crypto_box_keypair(pk,sk);
+
+	printf("PUBKEY: ");
+	for (i=0;i<32;++i) printf("%02x",pk[i]);
+	  printf("\n");
+
+	printf("SECKEY: ");
+	for (i=0;i<32;++i) printf("%02x",sk[i]);
+	  printf("\n");
+}
+
+void randombytes(char* bytes) {
+	           read(0,bytes,32);}
