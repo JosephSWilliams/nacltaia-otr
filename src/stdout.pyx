2cc7df000ec7334db6a9cf64f26827f335089a60
root <root@d3v11.ano>
2013-01-17 15:15:53 +0000

    [nacltaia-otr][stdout] alert on bad pm decrypt

--- src/stdout.pyx
+++ src/stdout.pyx
@@ -81,7 +81,9 @@ while 1:
       taias[src] = taia
 
       if m == 0:
-        buffer = ':CryptoServ!nacltaia-otr@service NOTICE ' + re.split(' +',buffer,3)[2] + ' :unable to decrypt message from ' + src
+        buffer = ':CryptoServ!nacltaia-otr@service NOTICE ' + re.split(' +',buffer,3)[2] + ' :unable to decrypt message from ' + src + '\a\n'
+        os.write(1,buffer)
+        continue
 
       else:
         buffer  = re.split(' +',buffer,1)[0] \
